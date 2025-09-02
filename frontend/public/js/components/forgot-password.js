a534ddd58e058cb920b91358cec74c51f78bb8dc
unknown <karensofia0404021@gmail.com>
2025-09-01 21:46:15 -0500

    feat: Componente de unit of measureÂ

--- frontend/public/js/components/forgot-password.js
+++ frontend/public/js/components/forgot-password.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const forgotContainer = document.querySelector(".forgot__container-wrapper");
+    const forgotContainer = document.querySelector(".forgot__container");
   
     if (forgotContainer) {
       fetch("/frontend/public/views/components/forgot-password.html")
